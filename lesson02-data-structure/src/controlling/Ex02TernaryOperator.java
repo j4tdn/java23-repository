89933a17c70000506646b7a4a2db40f819feb9e9
vphung <vphung2201@gmail.com>
2024-06-04 22:31:50 +0700

    lesson02-data-structure 02-06-2024

--- lesson02-data-structure/src/controlling/Ex02TernaryOperator.java
+++ lesson02-data-structure/src/controlling/Ex02TernaryOperator.java
@@ -42,5 +42,31 @@ public class Ex02TernaryOperator {
 		// Sử dụng toán tử 3 ngôi
 		String text = a > b ? ("Max là a = " + a) : ("Max là b = " + b);
 		System.out.println(text);
+	
+		// Xếp loại học lực
+		float roundedAvgPoint = 2.8f;
+		String ranking = "";
+		if (roundedAvgPoint < 5) {
+			ranking = "Yếu";
+		} else if (roundedAvgPoint < 6.5) {
+			ranking = "Trung bình";
+		} else if (roundedAvgPoint < 8) {
+			ranking = "Khá";
+		} else {
+			ranking = "Giỏi";
+		}
+		
+		System.out.println("Xếp loại ifelse --> " + ranking);
+		
+		ranking = roundedAvgPoint < 5 ? "Yếu" 
+												: roundedAvgPoint < 6.5 ? "Trung bình"
+																						: roundedAvgPoint < 8 ? "Khá"
+																														: "Giỏi";
+		System.out.println("Xếp loại ternary --> " + ranking);
+		
+		// Switch case, lopp(for, while, dowhile)
+		// Array(basic)
+		
 	}
+	
 }
