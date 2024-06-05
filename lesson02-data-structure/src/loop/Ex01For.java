681df8b02642cfd3a6d19b2017c62e3a602cadb7
xthanh <pxt28042003@gmail.com>
2024-06-05 22:07:27 +0700

    lesson02-data-structure 05.06.2024

---
+++ lesson02-data-structure/src/loop/Ex01For.java
@@ -0,0 +1,18 @@
+package loop;
+
+public class Ex01For {
+	
+	public static void main(String[] args) {
+		// for/while/do - while để
+		
+		// 1. duyệt các phần tử a đến b
+		// -> in ra danh sách các số nguyên từ 1 -> 10
+		for (int i = 1; i <= 10 ;i++) {
+			System.out.println("i -> " + i);
+		}
+		
+		
+		// 2. duyệt các phần tử trong danh sách / tập hợp
+	}
+
+}
---
+++ lesson02-data-structure/src/view/Ex05OperatorTests.java
@@ -0,0 +1,21 @@
+package view;
+
+public class Ex05OperatorTests {
+	public static void main(String[] args) {
+		// stack(ngăn xếp): LIFO: vào trước ra sau, vào sau ra trước --> ++i
+		// queue(hàng đợi): FIFO: vào trước ra trước, vào sau ra sau --> i++
+		// chuyển đổi từ số thập phân sang nhị phân
+		
+		int i = 2;
+		if (++i > 2 && i++ > 2) {
+			i++;
+		}
+		if (i++ > 4 || ++i > 5) {
+			System.out.println("i11 ->" + i);
+		}
+		if (i++ < 4 || ++i > 5) {
+			System.out.println("i14 -> " + i);
+		}
+	}
+	
+}
