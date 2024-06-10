f6a2e9705d6c4ba5a76e84d374348d4d99f1ffbd
Huynh Vu Hoang Nguyen <hvhn3295@gmail.com>
2024-06-10 18:23:38 +0700

    lesson02-huynhvuhoangnguyen 10.06.2024 add exercise 4 to 7

---
+++ lesson02-huynhvuhoangnguyen/src/view/Ex07DecimalToBinaryConverter.java
@@ -0,0 +1,12 @@
+package view;
+
+import java.util.Scanner;
+
+public class Ex07DecimalToBinaryConverter {
+	public static void main(String[] args) {
+		Scanner ip = new Scanner(System.in);
+		// Nhập số thập phân
+		System.out.println("Input decimal number: ");
+		int N = ip.nextInt();
+	}
+}
