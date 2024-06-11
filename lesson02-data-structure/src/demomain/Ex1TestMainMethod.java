cd1dc6c47e1934ea23095c42f96025a59107aad9
tungkhieu <tungkhieu2k@gmail.com>
2024-06-11 15:52:42 +0700

    lesson02-data-structure

---
+++ lesson02-data-structure/src/demomain/Ex1TestMainMethod.java
@@ -0,0 +1,11 @@
+package demomain;
+
+public class Ex1TestMainMethod {
+	// chú thích trên dòng
+	/*
+	 * 
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome TO Java23 Class");
+	}
+}
