ad667238b0d358c1acef68e5c6aaa789990f06f0
huan.hoang <huan0568@gmail.com>
2024-06-11 14:27:56 +0700

    lesson02-datastructure-11.06.2024

---
+++ lesson02-data-structure/src/bean/Item.java
@@ -0,0 +1,56 @@
+package bean;
+
+// Nhắc lại: class = data structure + methods
+
+// Khai báo KDL đối tượng, chứa các thông tin bên trong
+public class Item {
+	
+	// Item: Kiểu dữ liệu
+	
+	// class Item chứa 3 biến toàn cục id, name, salesPrice
+	// class Item chứa 3 thuộc tính id, name, salesPrice
+	
+	// lưu ý: non static
+	
+	// cứ 1 lần dùng từ khóa new gọi hàm khởi tạo để tạo ra ô nhớ để lưu trữ 1 Item
+	// thì ô nhớ đó luôn có 3 thuộc tính id, name, salesPrice với giá trị mặc định
+	public int id;
+	public char name;
+	public double salesPrice;
+	
+	// trong 1 class mặc định sẽ có 1 hàm khởi tạo rỗng
+	// khi gọi hàm khởi tạo này --> nó sẽ tạo ra 1 ô nhớ(chứa 3 thông tin id, name, salesPrice) ở vùng nhớ heap cho Item
+	// ô nhớ --> đối tượng, giá trị của item
+	
+	// Hàm khởi tạo, không có KDL trả về --> mặc định nó sẽ trả về KDL của class
+	// Tên hàm trùng vs tên class
+	public Item() {
+	}
+	
+	// Hàm khởi tạo, khởi tạo ô nhớ và cho phép gán giá trị cho các thuộc tính của ô nhớ đó
+	// Item i3 = new Item();
+	// i3.id=3;
+	// i3.name='C';
+	// i3.salesPrice=33;
+	
+	// Item i3 = new Item(3,'C',33)
+	
+	// sau khi tạo ô nhớ, biến this sẽ đại diện cho ô nhớ vừa tạo ra
+	// biến this đang trỏ đến ô nhớ
+	public Item(int pId, char pName, double pSalesPrice) {
+		this.id = pId;
+		this.name = pName;
+		this.salesPrice = pSalesPrice;
+	}
+	
+	// sout(i1); i1.toString()
+	// sout(i2); i2.toString()
+	
+	// i1.toString() --> this = i1
+	// i2.toString() --> this = i2;
+	@Override
+	public String toString() {
+		return this.id + ", " + this.name + ", " + this.salesPrice;
+	}
+	
+}
---
+++ lesson02-data-structure/src/datastructure/primitive/Ex01PrimitiveDemo.java
@@ -0,0 +1,36 @@
+package datastructure.primitive;
+
+public class Ex01PrimitiveDemo {
+	
+	public static void main(String[] args) {
+		
+		// khai báo
+		char letter = 'x';
+		int digit = 8;
+		long bigNum = 88;
+		float avgPoint = 8.6f;
+		double bigVal = 88.22d;
+		
+		// gọi sử dụng --> in ra
+		System.out.println("letter --> " + letter);
+		System.out.println("digit --> " + digit);
+		System.out.println("bigNum --> " + bigNum);
+		System.out.println("avgPoint --> " + avgPoint);
+		System.out.println("bigVal --> " + bigVal);
+		
+		// Biểu thức VT = VP --> gán giá tri của VP cho VT
+		int a = 44;
+		int b = 35;
+		int c = 88;
+		
+		a = b;
+		b = c;
+		c = 17;
+		c = a;
+		
+		System.out.println("a --> " + a); // 35
+		System.out.println("b --> " + b); // 88
+		System.out.println("c --> " + c); // 35
+	}
+	
+}
