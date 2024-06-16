fe0699936420942bdf6e7d91611b6848c5973299
Quyen Phan <Quyen.Phan@mgm-tp.com>
2024-06-16 22:13:45 +0700

    lesson03-oop 16.06.2024

---
+++ lesson03-oop/src/view/Ex06OrderCreator.java
@@ -0,0 +1,49 @@
+package view;
+
+public class Ex06OrderCreator {
+	
+	private String s1;
+	private static String s2;
+	
+	public static void main(String[] args) {
+		Ex06OrderCreator o1 = new Ex06OrderCreator();
+		o1.test01();
+		
+		// o1.test03();
+		Ex06OrderCreator.test03();
+	}
+	
+	// non-static
+	// chỉ thật sự được tạo ra khi có đối tượng tạo ra
+	public void test01() {
+		s1 = "hello";
+		s2 = "xin chào";
+	}
+	
+	public void test02() {
+		test04();
+	}
+	
+	// static
+	// load class 100% sẽ được khởi tạo
+	public static void test03() {
+		// s1 = "hello"; ko biết đối tượng nào cho s1
+		s2 = "xin chào";
+		test04();
+		// test02();
+	}
+	
+	public static void test04() {
+		// test02();
+		Ex06OrderCreator oz = new Ex06OrderCreator();
+		oz.test02();
+	}
+	
+	// static gọi static ==> ok
+	// non    gọi non    ==> ok
+	// non    gọi static ==> ok
+	
+	// static gọi non    ==> failed
+	// muốn gọi dc phải có/biết đối tượng gọi non-static method/attribute
+	
+}
