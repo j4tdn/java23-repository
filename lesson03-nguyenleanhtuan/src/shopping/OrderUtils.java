8972fd27480828615d25b9e1e4db95bdf2b3b6d6
Tuan Nguyen <anhtuanhdqb@gmail.com>
2024-06-22 19:01:16 +0700

    lesson03-nguyenleanhtuan 22.06.2024

---
+++ lesson03-nguyenleanhtuan/src/shopping/OrderUtils.java
@@ -0,0 +1,24 @@
+package shopping;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+public class OrderUtils {
+	private OrderUtils() {}
+	
+	public static double export(Order order) {
+		double totolOfMoney = 0;
+		ItemDetail[] ids = order.getItemDetails();
+		for(ItemDetail id:ids) {
+			Item item = id.getItem();
+			int quantity = id.getQuantity();
+			
+			double idCost = item.getCost()*quantity;
+			if(item.getCost() > 590 && LocalDate.of(2024, Month.MAY, 21).isEqual(order.getOrderDatime().toLocalDate())) {
+				idCost *= 0.9;
+			}
+			totolOfMoney += idCost;
+		}
+		return totolOfMoney;	
+	}
+}
