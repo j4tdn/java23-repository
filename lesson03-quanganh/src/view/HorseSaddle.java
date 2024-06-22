a772150f37b85001f218515a33b5d8a0e06fde5a
quanganh <anhpk2310@gmail.com>
2024-06-22 22:49:37 +0700

    lesson03-quanganh

---
+++ lesson03-quanganh/src/view/HorseSaddle.java
@@ -0,0 +1,71 @@
+package view;
+
+import java.util.Scanner;
+
+public class HorseSaddle {
+	int m,n;
+	int [][]H;
+	public static void main(String[] args) {
+		HorseSaddle hr = new HorseSaddle();
+	    hr.input();
+		hr.output();
+		hr.saddlepoint();
+	}
+	public void input() {
+		Scanner ip = new Scanner(System.in);
+			
+		System.out.print("số hàng ma trận: ");
+		m =  ip.nextInt();
+		System.out.print("số cột ma trận: ");
+		n =  ip.nextInt();
+		H = new int[m][n];
+		for(int i=0; i<m; i++) {
+			for(int j=0; j<n; j++) {
+				System.out.print("\nNhập giá trị cho mảng thứ - " + i+": ");
+				H[i][j] =  ip.nextInt();
+				
+			}
+		}
+	}
+	public int output() {
+		
+		System.out.println("\nMa trận: ");
+		for(int i=0; i<m; i++) {
+			for(int j=0; j<n; j++) {
+				System.out.print(H[i][j]+ "  ");
+			}
+			System.out.println();
+		}
+		return 0;
+	}
+public void saddlepoint() {
+    boolean saddlepoint = false;
+    
+    for (int i = 0; i < m; i++) {
+        int giatri = H[i][0];
+        int v = 0;
+
+        for (int j = 1; j < n; j++) {
+            if (H[i][j] < v) {
+            	giatri = H[i][j];
+                v = j;
+            }
+        }
+        boolean isSaddlePoint = true;
+        for (int k = 0; k < m; k++) {
+            if (H[k][v] > giatri) {
+                isSaddlePoint = false;
+                break;
+            }
+        }   
+        if (isSaddlePoint) {
+            System.out.println("\nPhần tử yên ngựa là: " + giatri );
+            saddlepoint = true;
+        }
+    }
+    if (!saddlepoint) {
+        System.out.println("\nNULL.");
+    }
+}
+}
+
