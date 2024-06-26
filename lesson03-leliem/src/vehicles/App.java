16e5c13e9f50ba111420b3879695855dda697f78
leliem <liemdz2605@gmail.com>
2024-06-26 09:51:51 +0700

    lesson03-leliem 26.06.2024

---
+++ lesson03-leliem/src/vehicles/App.java
@@ -0,0 +1,8 @@
+package vehicles;
+
+public class App {
+	public static void main(String[] args) {
+	        Vehicle[] vehicleArray = VehicleUtils.createVehicleArray();
+	        VehicleUtils.displayVehicleArray(vehicleArray);
+	}
+}
