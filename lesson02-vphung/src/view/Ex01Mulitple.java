181b6035020847df2aadc6a01d1ceedb7b1e7c5d
vphung <vphung2201@gmail.com>
2024-06-28 17:06:08 +0700

    lesson02-exercises 26-06

---
+++ lesson02-vphung/src/view/Ex01Mulitple.java
@@ -0,0 +1,24 @@
+package view;
+
+public class Ex01Mulitple {
+
+	/*
+	 Bài 1: Viết chương trình(hàm) kiểm tra số nguyên N có phải là bội của 2 hay không
+	Yêu cầu: N là số nguyên dương được nhập từ bàn phím, bắt buộc nhập lại nếu N không hợp lệ (tối
+	đa 5 lần)
+	VD: Nhập N = abc → báo lỗi, yêu cầu nhập lại
+	
+	= 4 → true
+	= 6 → true
+	= 7 → false
+	 */
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	private static boolean isMultipleOf2(int n) {
+		return n % 2 == 0;
+	}
+	
+}
