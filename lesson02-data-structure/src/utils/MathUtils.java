c19bab0b9cba3001a888da03b4ffac5da84ddbe5
Ngo Minh Quan <ngonghe0807@gmail.com>
2024-06-28 20:43:02 +0700

    lesson03-ngominhquan

---
+++ lesson02-data-structure/src/utils/MathUtils.java
@@ -0,0 +1,20 @@
+package utils;
+
+public class MathUtils {
+
+	private MathUtils() {
+	}
+	
+	// n = 5
+	public static long getFact(int n) {
+        long fact = n;
+        long running = n - 1;
+        while(running > 1) {
+        	fact 
+        }
+        
+		while(n > 1) {
+			fact *= n--;
+		}
+	} 
+}
