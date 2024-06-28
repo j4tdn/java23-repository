4514138e82d220adcd318f51b62aa4a34c41519f
Ngo Minh Quan <ngonghe0807@gmail.com>
2024-06-28 20:43:02 +0700

    lesson03-ngominhquan

---
+++ lesson02-data-structure/src/view/Ex03Fact.java
@@ -0,0 +1,11 @@
+package view;
+
+import utils.MathUtils;
+
+public class Ex03Fact {
+
+	public static void main(String[] args) {
+		int number = IoUtils.input(3);
+		System.out.println("f(" + number + ") = " + MathUtils.getFact(number));
+	}
+}
