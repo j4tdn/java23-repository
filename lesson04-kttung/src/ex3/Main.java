1f4ba4112e131cb8c2e186c29330c0613276e3d2
tungkhieu <tungkhieu2k@gmail.com>
2024-07-21 19:50:22 +0700

    lesson0

---
+++ lesson04-kttung/src/ex3/Main.java
@@ -0,0 +1,19 @@
+package ex3;
+
+public class Main {
+	public static void main(String[] args) {
+        int n = 5; 
+        printPascalTriangle(n);
+    }
+
+    public static void printPascalTriangle(int n) {
+        for (int i = 0; i < n; i++) {
+            int number = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(number + " ");
+                number = number * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+    }
+}
