24722782af7c5a5128277a4f3feb2a08f24ec340
vtai <taiduong.16102004@gmail.com>
2024-07-21 10:24:20 +0700

    d

--- lesson02-data-structure/src/datastructure/primitive/Ex01Primitivedemo.java
+++ lesson02-data-structure/src/datastructure/primitive/Ex01Primitivedemo.java
@@ -1,36 +1,18 @@
 package datastructure.primitive;
 
-public class Ex01Primitivedemo {
-
+public class Ex01PrimitiveDemo {
 	public static void main(String[] args) {
-		
-		//khai báo
 		char letter = 'x';
-		int digit = 3;
-		long bigNum = 159;
-		float avgPoint = 8.7f;
-		double bigVal = 159.87;
-		
-		//gọi sử dụng --> in ra
-		System.out.println("letter --> " + letter);
-		System.out.println("digit --> " + digit);
-		System.out.println("bigNum --> " + bigNum);
-		System.out.println("avgPoint --> " + avgPoint);
-		System.out.println("bigVal --> " + bigVal);
-		
-		//Biểu thức vế trái = vế phải --> gán giá trị của vế phải cho vế trái
-		VD: int a = 44;
-			int b = 35;
-			int c = 88;
-			
-			a = b;
-			b = c;
-			c = 17;
-			c = a;
-			
-			System.out.println("a --> " + a); // 35
-			System.out.println("b --> " + b); // 88 
-			System.out.println("c --> " + c); // 35
+		int digit = 8;
+		long bigNum = 88;
+		float avgPoint = 8.6f;
+		double bigVal = 88.22d;
+		System.out.println("letter --> " + letter  );
+		System.out.println("digit --> " + digit  );
+		System.out.println("bigNum --> " + bigNum  );
+		System.out.println("avgPoint --> " + avgPoint  );
+		System.out.println("bigVal --> " + bigVal  );
+
+
 	}
-	
 }
