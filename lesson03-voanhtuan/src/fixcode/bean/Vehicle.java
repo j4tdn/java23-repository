9e7ad68be5412d10a8f243be7b943990e7504a41
vatuan <anhtuan01012003@gmail.com>
2024-07-21 22:21:06 +0700

    lesson03-voanhtuan fix code 21.07.2024

---
+++ lesson03-voanhtuan/src/fixcode/bean/Vehicle.java
@@ -0,0 +1,86 @@
+package fixcode.bean;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+public class Vehicle {
+
+	private String owner;
+	private String model;
+	private Integer capacity;
+	private BigDecimal salesPrice;
+	
+	// private BigDecimal tax; // do tính toán ra(ko phải là input)
+
+	public Vehicle() {
+	}
+
+	public Vehicle(String owner, String model, Integer capacity, BigDecimal salesPrice) {
+		this.owner = owner;
+		this.model = model;
+		this.capacity = capacity;
+		this.salesPrice = salesPrice;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public Integer getCapacity() {
+		return capacity;
+	}
+	
+	public void setCapacity(Integer capacity) {
+		this.capacity = capacity;
+	}
+
+	public BigDecimal getSalesPrice() {
+		return salesPrice;
+	}
+	
+	public String getFormattedSalesPrice(DecimalFormat df) {
+		return df.format(salesPrice.doubleValue());
+	}
+
+	public void setSalesPrice(BigDecimal salesPrice) {
+		this.salesPrice = salesPrice;
+	}
+
+	public BigDecimal getTax() {
+		BigDecimal taxInPercent = null;
+		if (capacity < 100) {
+			taxInPercent = new BigDecimal("1");
+		} else if (capacity <= 200) {
+			taxInPercent = new BigDecimal("3");
+		} else {
+			taxInPercent = new BigDecimal("5");
+		}
+		return salesPrice.multiply(taxInPercent, MathContext.DECIMAL64)
+					.setScale(0, RoundingMode.HALF_UP);
+	}
+	
+	public String getFormattedTax(DecimalFormat df) {
+		return df.format(getTax().doubleValue());
+	}
+
+	@Override
+	public String toString() {
+		return "Vehicle [owner=" + owner + ", model=" + model + ", capacity=" + capacity + ", salesPrice=" + salesPrice
+				+ ", tax=" + getTax() + "]";
+	}
+
+}
---
+++ lesson03-voanhtuan/src/fixcode/view/Ex03SaddlePoint.java
@@ -0,0 +1,61 @@
+package fixcode.view;
+
+public class Ex03SaddlePoint {
+
+	public static void main(String[] args) {
+
+		int[][] matrix = { 
+			{ 47, 47, 36 }, // matrix[0]
+			{ 23, 43, 31 }, // matrix[1]
+			{ 46, 7, 29 }   // matrix[2]
+		};
+		
+		generate(matrix);
+		
+		System.out.println("Saddle point: " + getSaddlePoint(matrix));
+	}
+
+	private static int getSaddlePoint(int[][] matrix) {
+		int saddlePoint = -1;
+		forOut:
+		for (int rowI = 0; rowI < matrix.length; rowI++) {
+			for (int colI = 0; colI < matrix[rowI].length; colI++) {
+				int element = matrix[rowI][colI];
+				if(minRow(element, rowI, matrix) && maxCol(element, colI, matrix)) {
+					saddlePoint = element;
+					break forOut;
+				}
+			}
+		}
+		return saddlePoint;
+	}
+	
+	private static boolean maxCol(int checkElement, int colI, int[][] matrix) {
+		for (int rowI = 0; rowI < matrix.length; rowI++) {
+			if (matrix[rowI][colI] > checkElement) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private static boolean minRow(int checkElement, int rowI, int[][] matrix) {
+		int [] rowIElements = matrix[rowI];
+		for (int rowIElement : rowIElements) {
+			if (rowIElement < checkElement) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private static void generate(int[][] matrix) {
+		for (int rowI = 0; rowI < matrix.length; rowI++) {
+			for (int colI = 0; colI < matrix[rowI].length; colI++) {
+				System.out.printf("%-3d",matrix[rowI][colI]);
+			}
+			System.out.println();
+		}
+	}
+
+}
