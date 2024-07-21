fe66cffb9eac1a69a312f26ea4ce8f7d49642444
Thien Nguyen <thienleyi@gmail.com>
2024-07-21 22:31:51 +0700

    Update lesson04-exercises

--- lesson04-nguyenvanthien/src/humanresourcemanagement/HeadofDepartment.java
+++ lesson04-nguyenvanthien/src/humanresourcemanagement/HeadofDepartment.java
@@ -1,12 +1,22 @@
 package humanresourcemanagement;
 
-public class HeadofDepartment extends HumanResources {
-	private String TenDonVi;
-	private double HeSoChucVu;
-	
-	
+public class TruongPhong extends NhanSu {
+    private double heSoChucVu;
+    private String donVi;
 
-	
-	
-	
+    public TruongPhong(String hoTen, String ngaySinh, double heSoLuong, double heSoChucVu, String donVi) {
+        super(hoTen, ngaySinh, heSoLuong);
+        this.heSoChucVu = heSoChucVu;
+        this.donVi = donVi;
+    }
+
+    @Override
+    public double tinhLuong() {
+        return (heSoLuong + heSoChucVu) * 2200000;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Hệ số chức vụ: " + heSoChucVu + ", Đơn vị: " + donVi + ", Lương: " + tinhLuong();
+    }
 }
