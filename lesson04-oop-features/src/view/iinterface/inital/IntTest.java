4f385b984281dcf51432c09554480b40c436a85b
Quyen Phan <Quyen.Phan@mgm-tp.com>
2024-07-21 20:59:17 +0700

    lesson04-oop-features 21.07.2024

---
+++ lesson04-oop-features/src/view/iinterface/inital/IntTest.java
@@ -0,0 +1,12 @@
+package view.iinterface.inital;
+
+@FunctionalInterface
+public interface IntTest {
+	
+	boolean test(int number);
+	
+	default int doubleVal(int number) {
+		return number * number;
+	}
+	
+}
---
+++ lesson04-oop-features/src/view/iinterface/inital/StringTest.java
@@ -0,0 +1,13 @@
+package view.iinterface.inital;
+
+public interface StringTest {
+	
+	boolean isLowerCase(String text);
+	
+	boolean isUpperCase(String text);
+	
+	static boolean isEmpty(String text) {
+		return text.length() == 0;
+	}
+	
+}
