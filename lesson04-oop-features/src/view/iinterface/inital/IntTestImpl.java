bac479fcee228af910520f6bad2cb7aa775db570
Quyen Phan <Quyen.Phan@mgm-tp.com>
2024-07-21 20:59:17 +0700

    lesson04-oop-features 21.07.2024

---
+++ lesson04-oop-features/src/view/iinterface/inital/IntTestImpl.java
@@ -0,0 +1,12 @@
+package view.iinterface.inital;
+
+// Implementation class of interface
+public class IntTestImpl implements IntTest {
+
+	@Override
+	public boolean test(int number) {
+		System.out.println("IntTestImpl#test");
+		return number > 0;
+	}
+
+}
