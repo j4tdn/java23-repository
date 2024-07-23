d13f5f8e49f6293886dce2e33cee783f5371c8d5
ngominhquan <ngonghe0807@gmail.com>
2024-07-23 22:51:45 +0700

    ngominhquan237

---
+++ lesson04-ngominhquan/src/view/Ex03.java
@@ -0,0 +1,34 @@
+package view;
+
+import java.util.Scanner;
+
+public class Ex03 {
+	
+	public static void main(String[] args) {
+		int n;
+        int x;
+        int output;
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter hight of the triangle: ");
+        n = scanner.nextInt();
+ 
+        for(int i = 1;i<= n; i++){
+            for(int j = 0; j<= 2*n; j++){
+                x = j-n;
+                if(x < 0){
+                    x *= -1;
+                }
+                output = i - x;
+ 
+                if(output > 0){
+                    System.out.printf("%3d", output);
+                } else{
+                    System.out.printf("   ");
+                }
+            }
+            System.out.println();
+ 
+        }
+	}
+}
