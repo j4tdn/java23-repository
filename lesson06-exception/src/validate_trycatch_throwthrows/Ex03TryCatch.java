7de913c1a000b3c74ea931b0e5f057ba51922f22
Quyen Phan <Quyen.Phan@mgm-tp.com>
2024-08-11 22:39:17 +0700

    lesson06-exception 11.08.2024

---
+++ lesson06-exception/src/validate_trycatch_throwthrows/Ex03TryCatch.java
@@ -0,0 +1,36 @@
+package validate_trycatch_throwthrows;
+
+import java.time.Year;
+import java.util.Scanner;
+
+public class Ex03TryCatch {
+	
+	private static Scanner ip = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.print("Enter year of birth: ");
+		
+		// trong khối try bị exception gì thì catch block nên bắt đúng exception đó hoặc exception cha
+		// catch(e) có thể bắt cho chính nó hoặc con của nó
+		
+		// khi chưa có try/catch --> gặp exception --> dừng luôn chương trình
+		// int yob = Integer.parseInt(ip.nextLine());
+		int yob = 0;
+		
+		do {
+			try {
+				yob = Integer.parseInt(ip.nextLine()); // NumberFormatException
+				break;
+			} catch(RuntimeException e) { // RuntimeException e = NumberFormatException
+				System.out.println("error >> " + e.getMessage());
+				// hiển thị chi tiết exception giống như khi không xử lý ngoại lệ
+				// e.printStackTrace();
+				System.out.print("\nPlease enter a valid number: ");
+			}
+		} while(true);
+		
+		int age = Year.now().getValue() - yob + 1;
+		System.out.println("===> Your age = " + age);
+	}
+	
+}
