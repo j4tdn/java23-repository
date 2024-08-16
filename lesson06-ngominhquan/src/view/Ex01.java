6abca5acf582ef8377939e8f73c487a8f0b76766
ngominhquan <ngonghe0807@gmail.com>
2024-08-16 22:51:47 +0700

    lesson06-ngominhquan

--- lesson06-ngominhquan/src/view/Ex01.java
+++ lesson06-ngominhquan/src/view/Ex01.java
@@ -4,32 +4,53 @@ import java.util.Scanner;
 
 public class Ex01 {
 
-	public static boolean isValidEmail(String email) {
-        // Regular expression for valid email prefix
-        String emailRegex = "^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
-        
-        // Check if the email matches the regex
-        return email.matches(emailRegex);
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String email;
-
-        // Loop until a valid email is entered
-        while (true) {
-            System.out.print("Enter your email: ");
-            email = scanner.nextLine();
-
-            if (isValidEmail(email)) {
-                System.out.println("Valid email: " + email);
-                break;  // Exit the loop if the email is valid
-            } else {
-                System.out.println("Invalid email. Please try again.");
-            }
-        }
-        
-        scanner.close();
-    }
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		double k = 0;
+		double h = 0;
+		
+		do  {
+			try  {
+				System.out.print("Enter number a: ");
+				String a = sc.nextLine();
+				NumberChecker(a);
+				k = Double.parseDouble(a);
+				if (k == 0) {
+					System.out.println("a cannot be 0");
+				}
+				else { break; }
+			}
+			catch (NumberFormatException  e) {
+				System.out.println("Error: " + e.getMessage());
+			}
+		}
+		while (true);
+		
+		do  {
+			try  {
+				System.out.print("Enter number b: ");
+				String b = sc.nextLine();
+				NumberChecker(b);
+				h = Double.parseDouble(b);
+				break;
+			}
+			catch (NumberFormatException  e) {
+				System.out.println("Error: " + e.getMessage());
+			}
+		}
+		while (true);
+		
+		double x = -k/h;
+		
+		System.out.print("Value of x: " + x);
+		
+	}
+	
+	public static void NumberChecker (String a) throws NumberFormatException {
+		
+		if (!a.matches("\\d+")) {
+			throw new NumberFormatException("a must be a number");
+		}
+	}
 	
 }
