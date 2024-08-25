e4872c1e9fde816fd9119d09f69ba8fca32e7910
vatuan <anhtuan01012003@gmail.com>
2024-08-25 16:21:57 +0700

    lesson06-exception 11.08.2024

---
+++ lesson06-exception/src/validate_trycatch_throwthrows/Ex05ThrowThrows.java
@@ -0,0 +1,28 @@
+package validate_trycatch_throwthrows;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Ex05ThrowThrows {
+
+	public static void main(String[] args) {
+		
+		try {
+			createFile("java23_test.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("finished");
+		
+	}
+	
+	private static File createFile(String path) throws IOException {
+		File file = new File(path);
+		if (!file.exists()) {
+			file.createNewFile(); // hàm có sẵn của Java
+		}
+		return file;
+	}
+	
+}
