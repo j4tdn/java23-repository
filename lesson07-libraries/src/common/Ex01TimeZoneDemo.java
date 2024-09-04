703d4ae8416bfa6a1de179dc9fad9c56a691c4f6
vatuan <anhtuan01012003@gmail.com>
2024-09-04 16:48:34 +0700

    lesson07-libraries update 25.08.2025

---
+++ lesson07-libraries/src/common/Ex01TimeZoneDemo.java
@@ -0,0 +1,19 @@
+package common;
+
+public class Ex01TimeZoneDemo {
+	
+	public static void main(String[] args) {
+		
+		// TimeZone
+		// Là thời gian được xác định ở khu vực, vị tr
+		// Là khái niệm dùng để quy ước, ước tính thời gian tại mỗi quốc gia, khu vực trên thế giới
+		
+		// GMT5
+		
+		// UTC
+		
+		// Area/Zone
+		
+	}
+
+}
---
+++ lesson07-libraries/src/localdatetime/Ex01LocalDateDemo.java
@@ -0,0 +1,61 @@
+package localdatetime;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class Ex01LocalDateDemo {
+
+	// Date : new
+	//		: setter -> void
+
+	// Calendar: Calendar.getInstance() --> singleton
+
+	// localDate: --> builder
+
+	public static void main(String[] args) {
+
+		// month: 1-12
+
+		LocalDate ldate = LocalDate.now();
+		System.out.println("ldate: " + ldate);
+
+		// Enum? hoặc number cho tháng
+		LocalDate ldate1 = LocalDate.of(2025, Month.JUNE, 18);
+		System.out.println("\nldate1: " + ldate1);
+
+		LocalDate ldate2 = LocalDate.now().withDayOfMonth(12).withYear(2022).plusWeeks(1); // 19/08/2022
+		System.out.println("\nldate2: " + ldate2); // toString
+
+		// format --> DateTimeFormatter
+		// LocalDate -> String
+		DateTimeFormatter df = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+		System.out.println("ldate2 format: " + df.format(ldate2));
+
+		// String -> LocalDate
+		LocalDate ldate3 = LocalDate.parse("10.10.2020", df);
+		System.out.println("ldate3 parse: " + ldate3);
+
+		System.out.println("\n=======================\n");
+
+		LocalDate start = LocalDate.parse("15.10.2022", df);
+		LocalDate end = LocalDate.parse("22.08.2024", df);
+
+		// Hiệu của LocalDate: Period
+		Period period = Period.between(start, end);
+		System.out.println("period: " + period); // toString
+		System.out.println("period details: " + 
+			    opt(period.getYears(), "year") +
+			    opt(period.getMonths(), "month") +
+			    opt(period.getDays(), "day"));
+	}
+
+	private static String opt(long value, String unit) {
+		if (value <= 0) {
+			return "";
+		}
+		return value + " " + unit + (value > 1 ? "(s)" : "") + " ";
+	}
+
+}
