697e9521a6a57957a7ee1c8d74580a8e741a78e8
xthanh <pxt28042003@gmail.com>
2024-09-08 09:32:17 +0700

    lesson08-string 04.09.2024

---
+++ lesson08-string/src/string/mutable/Ex01StringMutable.java
@@ -0,0 +1,29 @@
+package string.mutable;
+
+public class Ex01StringMutable {
+	
+	public static void main(String[] args) {
+		// Concatenate --> String Immutable
+		String[] sequences = {"a", "b", "c", "d", "e"};
+		
+		// Concatenate --> String Immutable
+		String s1 = "";
+		for (String sequence: sequences) {
+			s1 = s1 + sequence + "";
+			System.out.println("s1 code --> " + System.identityHashCode(s1));
+		}
+		System.out.println("s1 --> " + s1);
+		
+		System.out.println("\n");
+		
+		// Concatenate --> String Mutable(String Builder, String Buffer)
+		StringBuffer s2 = new StringBuffer(); // StringBuilder/StringBuffer: String(tt)
+		for (String sequence: sequences) {
+			s2 = s2.append(sequence).append("");
+			System.out.println("s2 code --> " + System.identityHashCode(s2));
+		}
+		System.out.println("s2 --> " + s2);
+		
+	}
+	
+}
