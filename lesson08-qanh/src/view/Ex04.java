5f218bd2b26f8ba6bb1014d78ec0e1a8a2eed426
quanganh <anhpk2310@gmail.com>
2024-09-08 22:46:01 +0700

    lesson08-qanh3

--- lesson08-qanh/src/view/Ex04.java
+++ lesson08-qanh/src/view/Ex04.java
@@ -1,5 +1,34 @@
 package view;
 
-public class Ex04 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Ex04 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("nhap vao một số nguyên 1 : ");
+		long number1 = sc.nextInt();
+		System.out.println("nhap vao môt số nguyên 2 : ");
+		long numbre2 = sc.nextInt();
+		
+		
+	}
+	private static boolean IsaPrime(long number1 , long number2) {
+		return Arrays.equals(getDividedPrimeNumber(number1),getDividedPrimeNumber(number2));
+	}
+	private static long[] getDividedPrimeNumber(long number) {
+		long[] divisible = new long[Integer.MAX_VALUE];
+		int count = 0;
+		for(long i = 2 ; i<= number/2 ; i++) {
+			if(number % i == 0 && IsPrime()) { 
+				
+			}
+			
+		}
+	}
+	private static boolean isprime(long n1 ) {
+		
+	}
+	
 }
--- lesson08-qanh/src/view/Ex06.java
+++ lesson08-qanh/src/view/Ex06.java
@@ -2,4 +2,19 @@ package view;
 
 public class Ex06 {
 
+	public static void main(String[] args) {
+		
+		int findingPos =100;
+		int running = 1;
+		StringBuilder sb = new StringBuilder();
+		
+		while (true) {
+			sb = sb.append(running);
+			if(sb.length()>= findingPos) {
+				break;
+			}
+			running++;
+		}
+		System.out.println("sb --> "+ sb.charAt(findingPos-1));
+	}
 }
