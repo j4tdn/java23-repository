2c906287abef757befa45cf52e89a0fc399fe21f
Thien Nguyen <thienleyi@gmail.com>
2024-10-03 10:34:13 +0700

    lesson10-arrays 03/10/2024

---
+++ lesson10-arrays/src/functional/IntTest.java
@@ -0,0 +1,8 @@
+package functional;
+
+@FunctionalInterface
+public interface IntTest {
+
+	boolean strategy(int number);
+
+}
---
+++ lesson10-arrays/src/view/Ex04OperationsS.java
@@ -0,0 +1,42 @@
+package view;
+
+public class Ex04OperationsS {
+	
+	/*
+	  
+	  Cho 1 mảng số nguyên gồm n phần tử 
+	  Viết chương trình
+	  
+	  1.Tìm tổng các phần tử trong mảng
+	  2.Tìm tích các phần tử trong mảng
+	  3.Tìm giá trị lớn nhất của các phần tử trong mảng
+	  4.Tìm giá trị nhỏ nhất của các phần tử trong mảng
+	  
+	 */
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	private static int sum(int[] elements) {
+		int sum= 0;
+		
+		for(int element: elements) {
+			sum = sum +element;
+		}
+		return sum;
+	}
+}
+//for (int element : elements) {
+//    sum += element;
+//    mul *= element;
+//
+//    if (element > max) {
+//        max = element;
+//    }
+//
+//    if (element < min) {
+//        min = element;
+//    }
+//}
+
