e8d072fe8d590f205db5b54bfc9f96474e6dd804
leliem <liemdz2605@gmail.com>
2024-10-27 22:01:59 +0700

    lession11-leliem 27.10.2024

---
+++ lession11-leliem/src/view/Ex01TransationManager.java
@@ -0,0 +1,14 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.Transaction;
+
+public class Ex01TransationManager {
+
+	public static void main(String[] args) {
+		List<Transaction> transaction2011 = new ArrayList<Transaction>();
+		for(Transaction transaction )
+	}
+}
