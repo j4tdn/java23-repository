f66b05b9e0851e25b82a5dd5150a2bf918aa99f6
Thien Nguyen <thienleyi@gmail.com>
2024-10-27 23:22:38 +0700

    nguyenvanthien em quen save nen bi thieu 1 chut trong bai

--- lesson11-nguyenvanthien/src/ex01/Trader.java
+++ lesson11-nguyenvanthien/src/ex01/Trader.java
@@ -1,5 +1,37 @@
 package ex01;
 
 public class Trader {
-
+	private String id;
+	private String name;
+	private String  city;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public Trader(String id, String name, String city) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Trader [id=" + id + ", name=" + name + ", city=" + city + "]";
+	}
+	
+	
 }
--- lesson11-nguyenvanthien/src/ex01/TraderTransaction.java
+++ lesson11-nguyenvanthien/src/ex01/TraderTransaction.java
@@ -1,5 +1,6 @@
 package ex01;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -28,16 +29,36 @@ public class TraderTransaction {
 	// function
 
 	public static List<Transaction> getTransactionsfind2011(List<Transaction> transactions) {
-		return transactions.stream()
-				.filter(t -> t.getYear() == 2011)
+		return transactions.stream().filter(t -> t.getYear() == 2011)
 				.sorted(Comparator.comparingInt(Transaction::getValue)).collect(Collectors.toList());
 	}
-	
 
-//	public static void main(String[] args) {
-////		Trader trader1= new Trader("1", "Thieenj", "Milan");
-////		Trader trader2= new Trader("2", "Nguyen", "VietNam");
-////		Trader trader3= new Trader("3", "Van", "Lao");
-////		Trader trader4= new Trader("4", "Bo", "China");
-//	}
+	public static List<Transaction> getHightValue(List<Transaction> transactions) {
+		return transactions.stream().filter(t -> t.getValue() > 300)
+				.sorted(Comparator.comparing(t -> t.getTrader().getCity())).collect(Collectors.toList());
+	}
+
+	public static List<Integer> getCambridge(List<Transaction> transactions) {
+		return transactions.stream().filter(t -> "Cambridge".equals(t.getTrader().getCity())).map(Transaction::getValue)
+				.collect(Collectors.toList());
+	}
+
+	public static void main(String[] args) {
+
+		
+		Trader trader1= new Trader("1", "Thieenj", "Milan");
+		Trader trader2= new Trader("2", "Nguyen", "Cambridge");
+		Trader trader3= new Trader("3", "Van", "Lao");
+		Trader trader4= new Trader("4", "Bo", "China");
+		
+		List<Transaction> transactions = Arrays.asList(
+				new Transaction("1",trader1,2012,"Item1",400),
+				new Transaction("2",trader2,2011,"Item2",300),
+				new Transaction("3",trader3,2013,"Item3",200),
+				new Transaction("4",trader4,2014,"Item4",1590)
+			
+				);
+		Set<String> trader
+				
+	}
 }
