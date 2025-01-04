1ff1704b23f77d78ac973c0a54ff8427e1b66118
vtai <taiduong.16102004@gmail.com>
2025-01-04 23:05:45 +0700

    lesson14-thread

---
+++ lesson14-thread/src/deadlock/SuspendResumeDeadlock.java
@@ -0,0 +1,36 @@
+package deadlock;
+
+public class SuspendResumeDeadlock {
+	private static Object data = new Object();
+		public static void main(String[] args) throws InterruptedException {
+			
+			final Thread thread1 = new Thread("Thread-1") {
+				public void run() {
+					System.out.println(Thread.currentThread().getName() + "has started. ");
+					//synchronized data
+					synchronized (data) {
+						System.out.println(Thread.currentThread().getName() + "has obtanined lock on data " + " & suspended...");
+						
+						Thread.currentThread().suspend(); //pause
+						System.out.println(Thread.currentThread().getName() + "has started lock on data");
+					}
+					System.out.println(Thread.currentThread().getName() + " has ENDED.");
+				}
+			};
+			thread1.start();
+			Thread.sleep(2000);
+			final Thread thread2 = new Thread("Thread-2") {
+				public void run() {
+					System.out.println(Thread.currentThread().getName() + "has started. ");
+					System.out.println(Thread.currentThread().getName() + "is trying to obtain lock on data");
+					thread1.resume();
+					synchronized (data) {
+						System.out.println(Thread.currentThread().getName() + "has obtanined lock on data ");
+						System.out.println(Thread.currentThread().getName() + "has released lock on data");
+					}
+					System.out.println(Thread.currentThread().getName() + " has ENDED.");
+				}
+			};
+			thread2.start();
+		}
+}
---
+++ lesson14-thread/src/ultil/ThreadUtils.java
@@ -0,0 +1,40 @@
+package ultil;
+
+import java.util.concurrent.TimeUnit;
+
+public class ThreadUtils {
+	private ThreadUtils() {
+	}
+	public static void printCurrentThreadName() {
+		System.out.println(">> # thread # " + Thread.currentThread().getName());
+	}
+	public static void startThread() {
+		System.out.println( Thread.currentThread().getName() + "is running...");
+	}
+	public static void startThread(long time) {
+		System.out.println( Thread.currentThread().getName() + "is running..." + time);
+	}
+	
+	// virtual task with a certain time
+	public static void doTask(long time, TimeUnit unit) {
+		try {
+			unit.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	public static String getThreadName() {
+		return Thread.currentThread().getName();
+	}
+	public static void endThread(long start) {
+		System.out.println(Thread.currentThread().getName() + "took " + (System.currentTimeMillis()- start) + "ms");
+	}
+	// block current thread 
+	public static void join(Thread thread) {
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
