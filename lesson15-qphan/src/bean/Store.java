b8c986d22cb6668a47cb53eb0bf1fdc837b3265e
Quyen Phan <Quyen.Phan@mgm-tp.com>
2025-01-12 19:34:41 +0700

    initial data 12.01.2025

--- lesson15-qphan/src/bean/Store.java
+++ lesson15-qphan/src/bean/Store.java
@@ -3,6 +3,7 @@ package bean;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+<<<<<<< HEAD
 public class Store {
 	private Long storeId;
 	private Long referenceStoreId;
@@ -65,10 +66,71 @@ public class Store {
 
 	@Override
 	public boolean equals(final Object o) {
+=======
+/**
+ * Store: Cửa hàng
+ * + id: mã cửa hàng
+ * + desc: mô tả
+ * + potential: số mặt hàng cần có để bán
+ * + whId: mã kho, kho hàng chuyển mặt hàng đến cửa hàng
+ */
+public class Store {
+
+	private Integer id;
+	private String desc;
+	private BigDecimal potential;
+	private Integer whId;
+
+	public Store() {
+	}
+
+	public Store(Integer id, String desc, BigDecimal potential, Integer whId) {
+		this.id = id;
+		this.desc = desc;
+		this.potential = potential;
+		this.whId = whId;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public BigDecimal getPotential() {
+		return potential;
+	}
+
+	public void setPotential(BigDecimal potential) {
+		this.potential = potential;
+	}
+
+	public Integer getWhId() {
+		return whId;
+	}
+
+	public void setWhId(Integer whId) {
+		this.whId = whId;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+>>>>>>> fccdfd1 (initial data 12.01.2025)
 		if (this == o) {
 			return true;
 		}
 
+<<<<<<< HEAD
 		if (o instanceof Store) {
 			return false;
 		}
@@ -76,16 +138,35 @@ public class Store {
 		final Store store = (Store) o;
 
 		return getStoreId().equals(store.getStoreId());
+=======
+		if (!(o instanceof Store)) {
+			return false;
+		}
+
+		Store that = (Store) o;
+
+		return getId().equals(that.getId());
+>>>>>>> fccdfd1 (initial data 12.01.2025)
 	}
 
 	@Override
 	public int hashCode() {
+<<<<<<< HEAD
 		return Objects.hash(storeId);
+=======
+		return Objects.hash(getId());
+>>>>>>> fccdfd1 (initial data 12.01.2025)
 	}
 
 	@Override
 	public String toString() {
+<<<<<<< HEAD
 		return "Store [storeId=" + storeId + ", referenceStoreId=" + referenceStoreId + ", stockPreviousDay="
 				+ stockPreviousDay + ", expectedSales=" + expectedSales + ", isSelected=" + isSelected + "]";
 	};
 }
+=======
+		return "Store [id=" + id + ", desc=" + desc + ", potential=" + potential + ", whId=" + whId + "]";
+	}
+}
+>>>>>>> fccdfd1 (initial data 12.01.2025)
