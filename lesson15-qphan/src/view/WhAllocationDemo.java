fccdfd1b1df316aa307a7931c059b358e1f903b0
Quyen Phan <Quyen.Phan@mgm-tp.com>
2025-01-12 19:34:41 +0700

    initial data 12.01.2025

---
+++ lesson15-qphan/src/view/WhAllocationDemo.java
@@ -0,0 +1,14 @@
+package view;
+
+public class WhAllocationDemo {
+	
+	public static void main(String[] args) {
+		// use data from DataModel
+		
+		// do calculation
+		
+		// print result step by step
+	}
+	
+	
+}
