dd6b5ecf60f5e7b37488118a1a118b410d398375
vatuan <anhtuan01012003@gmail.com>
2025-03-07 19:27:26 +0700

    Lesson 16 - Database - 07.03.2025 - Querries

---
+++ 2. Shopping DB patches/00. java23-shopping database patches.sql
@@ -0,0 +1,25 @@
+-- creat database/sche
+CREATE DATABASE java23_shopping CHAR SET utf8mb4;
+
+-- step by step to create database patches
+
+-- java23-shopping dbp 01.sql
+-- T04_ITEM_GROUP 
+
+-- java23-shopping dbp 02.sql
+-- T01_ITEM	 T02_SIZE  T03_ITEM_DETAIL T05_GALLERY
+
+-- java23-shopping dbp 03.sql
+-- C09_ACCOUNT_ID	C10_ROLE_ID
+
+-- java23-shopping dbp 04.sql
+-- C07_CUSTOMER_ID C08_EMPLOYEE_ID	 C14_TITLE_ID
+
+-- java23-shopping dbp 05.sql
+-- C06_ORDER_ID C13_PAYMENT_METHOD_ID	C15_BILL_ID	C16_ORDER_ID
+
+-- java23-shopping dbp 06.sql
+-- C11_ORDER_STATUS_ID	C12_ORDER_ID
+
+-- java23-shopping dbp 07.sql
+-- C17_IRN_ID	C18_IRN_ID	C19_PROVIDER_ID
