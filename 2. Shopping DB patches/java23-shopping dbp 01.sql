fb1a8e42c27b3549ec45512fdc08cc50b86a86cc
Trung Nguyen <hongtrung2973@gmail.com>
2025-03-07 20:46:23 +0700

    lesson16-database-hongtrung 07.03.2025

---
+++ 2. Shopping DB patches/java23-shopping dbp 01.sql
@@ -0,0 +1,20 @@
+-- create database/ schema
+CREATE DATABASE java23_shopping CHAR SET utf8mb4;
+
+-- step by step to create database patches
+
+USE java23_shopping;
+
+-- java23-shopping dbp 01.sql
+-- T04_ITEM_GROUP
+
+-- ID là số thường có 2 cách tạo ra giá trị
+-- Đơn giản: AUTO_INCREMENT -> tự động tăng 1 đơn vị nếu ko insert giá trị cụ thể
+-- Cách khác: tạo ra sequence để khai báo sau mỗi lần tăng bao nhiêu đơn vị( ko support trong mysql)
+
+CREATE TABLE T04_ITEM_GROUP(
+	C04_ITEM_GROUP_ID INT AUTO_INCREMENT PRIMARY KEY,
+    C04_ITEM_GROUP_NAME VARCHAR(255) NOT NULL,
+    CONSTRAINT UNQ_C02_ITEM_GROUP_NAME UNIQUE(C04_ITEM_GROUP_NAME)
+);
+
