2e69d20266b40088e20a4a2fcd62f1fb97117d33
hvhnguyen <hvhn3295@gmail.com>
2025-04-09 22:13:15 +0700

    lesson16-database 09.04.2025

---
+++ 4. Refresh Test Data/java23-shopping f_random.sql
@@ -0,0 +1,11 @@
+DROP FUNCTION IF EXISTS f_random;
+
+DELIMITER $$
+
+-- random a number from 1(inclusive) to maxInclusive
+
+CREATE FUNCTION f_random(maxInclusive INT)
+RETURNS INT
+BEGIN
+    RETURN floor(rand() * maxInclusive + 1);
+END $$
