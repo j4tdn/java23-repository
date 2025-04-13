88a25f0dc70b2d977c27d661a1a9ffa513211173
unknown <hoangut20172018@gmail.com>
2025-04-13 21:20:05 +0700

    finishing creating test data - 13.04.2025

---
+++ 4. Refresh Test Data/utils/java23-shopping 03 p_insert_into_customer.sql
@@ -0,0 +1,38 @@
+DROP PROCEDURE IF EXISTS p_insert_into_customer;
+
+DELIMITER $$
+
+CREATE PROCEDURE p_insert_into_customer()
+BEGIN
+    -- Khai báo biến
+	DECLARE max INT DEFAULT 10;
+    DECLARE running INT DEFAULT 1;
+
+    WHILE running <= max DO
+        INSERT INTO T07_CUSTOMER(
+			C07_CUSTOMER_ID,
+            C07_CUSTOMER_NAME,
+            C07_CUSTOMER_PHONE,
+            C07_CUSTOMER_EMAIL,
+            C07_CUSTOMER_GENDER,
+            C07_CUSTOMER_DOB,
+            C07_CUSTOMER_ADDRESS,
+            C07_USERNAME,
+            C07_PASSWORD,
+            C07_ACCOUNT_STATUS
+        )
+        VALUES(
+			running,
+            concat('Khách Hàng ', running),
+            concat(running, '123456789'),
+            concat('kh', running, '@gmail.com'),
+            f_cast_to_bit(elt(f_random(2), 0, 1)),
+            DATE_SUB(DATE_SUB(current_timestamp(), INTERVAL running YEAR), INTERVAL running DAY),
+            concat('Địa chỉ ', running),
+            concat('kh', running, if(running MOD 2 != 0,  'def', 'auto')),
+            '$2a$12$w0bs0MW/O3nTyMhuv0r1jOjq2gOaxLxkZgms7u/khHRmtCh3S/Hpu',
+            1
+		);
+		SET running = running + 1;
+    END WHILE;
+END $$
---
+++ 4. Refresh Test Data/utils/java23_shopping 01  f_cast_to_bit.sql
@@ -0,0 +1,8 @@
+DROP FUNCTION IF EXISTS f_cast_to_bit;
+
+DELIMITER $$
+
+CREATE FUNCTION f_cast_to_bit (val INT) RETURNS BIT(1)
+BEGIN
+    RETURN val;
+END
